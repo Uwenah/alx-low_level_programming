a5b4ae90523bcc1d1256febbc5472d37e94fcbcf
Uwenah <uwenah81@gmail.com>
2023-06-16 04:00:36 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include<stdio.>
+#include<stdio.h>
+/**
+* main - A program that prints a line with put fuction
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-{       
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
 }
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
