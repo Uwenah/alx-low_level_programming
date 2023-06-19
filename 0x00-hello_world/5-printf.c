7ba7fdccabdff46039a2ca1c1065e8011529a9fd
Uwenah <uwenah81@gmail.com>
2023-06-19 10:05:35 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-}
-     puts("with proper grammar, but the outcome is a piece of art,\n");
-     return (0);
+{
+puts("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
