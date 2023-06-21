42327a268bf99d96379a45d18cc8c3862abc98fe
Uwenah <uwenah81@gmail.com>
2023-06-21 06:37:57 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print 10 times the alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (i = 1; i <=10; i++)
+{
+for (j = 97; j <=122; j++)
+_putchar(j);
+}
+_putchar('\n');
+}
