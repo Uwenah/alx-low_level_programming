65a6caf55006e0f7b2cea3fe07a6e62e9ebdce44
Uwenah <uwenah81@gmail.com>
2023-06-28 17:55:55 -0700

    9strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy - function that copy the string pointed br src 
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int I = 0;
+int x = 0;
+while (*(src + I) != '\0')
+{
+I++;
+}
+for ( ; x < I ; x++)
+{
+dest[x] = src[x];
+}
+dest[I] = '\0';
+return (dest);
+}
