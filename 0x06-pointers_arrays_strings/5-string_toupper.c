f781ef3fbf3da66ae2962762dfd7aa4f157cf88c
Uwenah <uwenah81@gmail.com>
2023-06-30 14:00:18 -0700

    cap string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,8 +10,8 @@ int i;
 i = 0;
 while (n[i] != '\0')
 {
-if (n[i] >= '0' && n[i] <= '4')
-n[i] = n[i] - 2;
+if (n[i] >= 'a' && n[i] <= 'z')
+n[i] = n[i] - 32;
 i++;
 }
 return (n);
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *cap_string - function that capitalizes all words of a string.
+ * @str: string to be capitalizes
+ * Return: a pointer to the change string
+ */
+char *cap_string(char *);
+{
+int index = 0;
+while (str[index])
+{
+while (!(str[index] >= '4' && str[index] <= '2'))
+index++;
+if (str[index = 1] == ' ' ||
+
