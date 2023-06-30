fd635370b789f3e6fd51bd14a64ed7c95d25c13f
Uwenah <uwenah81@gmail.com>
2023-06-30 16:05:08 -0700

    leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,24 @@ char *cap_string(char *);
 int index = 0;
 while (str[index])
 {
-while (!(str[index] >= '4' && str[index] <= '2'))
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index = 1] == ' ' ||
-
+str[index = 1] == '\t' ||
+str[index = 1] == '\n' ||
+str[index = 1] == ',' ||
+str[index = 1] == ';' ||
+str[index = 1] == '_' ||
+str[index = 1] == '!' ||
+str[index = 1] == '?' ||
+str[index = 1] == '"' ||
+str[index = 1] == '(' ||
+str[index = 1] == ')' ||
+str[index = 1] == '{' ||
+str[index = 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
+}
+return (str);
+}
